3aec375cf0f536e4393b073c9d8ecb6a41c09f36
VityaShevcov <vitya.shevcov@gmail.com>
2023-01-05 01:42:22 +0300

    add test

---
+++ test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from main import app
+
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+    
+    
+def test_read_predict_positive():
+    response = client.post("/predict/",
+        json={"text": "альфабанк"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data == 'альфабанк» (г. Москва) и «Сбербанк России» (г.'
